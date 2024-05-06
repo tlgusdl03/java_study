99398e6a74ed8925f980caf74891bd29e408f617
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2024-05-06 22:22:56 +0900

    feat: 변수 학습

---
+++ src/variable/VarEx3.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long l = 10000000000L;
+        System.out.println(l);
+        boolean b = true;
+        System.out.println(b);
+    }
+}
