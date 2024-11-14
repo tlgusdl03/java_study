6d10502dd53999b089d0007027d1909933ea086a
tlgusdl03 <tlgusdl03@gmail.com>
2024-11-14 19:34:17 +0900

    feat : Swing GUI환경에서 CRUD 기능 구현

--- src/final_assignment/practice/CRUDPractice.java
+++ src/final_assignment/practice/CRUDPractice.java
@@ -1,72 +1,15 @@
 package final_assignment.practice;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Scanner;
 
 // Test implementation of CRUD of program Using File I/O Instead of Using DB
 // After CLI test proceeded, Also will test on GUI
-class EntireScreen extends JFrame{
-    private Container c = getContentPane();
-    public EntireScreen(){
-        setTitle("GUI CRUD TEST");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        c.setLayout(new BorderLayout());
-        pack();
-
-        c.add(new InputHeader(), BorderLayout.NORTH);
-        c.add(new Body(), BorderLayout.CENTER);
-
-        setVisible(true);
-    }
-}
-
-class InputHeader extends JPanel{
-    public InputHeader(){
-        setSize(500, 100);
-        setLayout(null);
-
-        JLabel choiceLabel = new JLabel("Enter the choice");
-        JTextField choiceInput = new JTextField("Ex: 1 or 2 or....", 1);
-
-        JLabel nameLabel = new JLabel("Enter the Name");
-        JTextField nameInput = new JTextField("Ex: Steve, John...", 1);
-
-        JLabel ageLabel = new JLabel("Enter the Age");
-        JTextField ageInput = new JTextField("Ex: 37, 23...", 1);
-
-        choiceLabel.setBounds(10,0,100, 30);
-        choiceInput.setBounds(10, 50, 100, 30);
-        nameLabel.setBounds(110, 0, 100, 30);
-        nameInput.setBounds(110, 50, 100, 30);
-        ageLabel.setBounds(210, 0, 100, 30);
-        ageInput.setBounds(210, 50, 100, 30);
-
-        add(choiceInput);
-        add(choiceInput);
-        add(nameLabel);
-        add(nameInput);
-        add(ageLabel);
-        add(ageInput);
-
-        setVisible(true);
-    }
-}
-
-class Body extends JPanel{
-    public Body(){
-        setLayout(null);
-        setSize(500, 500);
-
-        JScrollPane jScrollPane = new JScrollPane();
-    }
-}
-
 class Person{
     public String name;
     public int age;
@@ -91,6 +34,12 @@ class Person{
 public class CRUDPractice {
     private ArrayList<Person> people = new ArrayList<>();
     private final String filePath = "src/final_assignment/practice/text.txt";
+    private Body body;
+
+    public CRUDPractice(){
+        initialize();
+        new EntireScreen();
+    }
 
     public void initialize(){
         File file = new File(filePath);
@@ -117,6 +66,9 @@ public class CRUDPractice {
             }
             bufferedReader.close();
             fileReader.close();
+            if(body != null){
+                body.updateList();
+            }
         }catch (IOException e){
             e.printStackTrace();
         }
@@ -124,16 +76,9 @@ public class CRUDPractice {
 
     public void createPerson(String name, int age){
         Person newPerson = new Person(name, age);
-        try{
-            FileWriter fileWriter = new FileWriter(filePath, true);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(newPerson.toString());
-            bufferedWriter.newLine();
-            bufferedWriter.close();
-            fileWriter.close();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        people.add(newPerson);
+        saveToFile();
+        body.updateList();
     }
 
     public void updatePerson(String name, int newAge){
@@ -146,10 +91,9 @@ public class CRUDPractice {
                 break;
             }
         }
-
         if(found){
             saveToFile();
-            System.out.println(name +"`s age is updated to " + newAge  );
+            body.updateList();
         }else{
             System.out.println(name + "is not found");
         }
@@ -168,7 +112,7 @@ public class CRUDPractice {
         }
         if (found) {
             saveToFile();
-            System.out.println(name + "is deleted");
+            body.updateList();
         } else {
             System.out.println(name + "is not found");
         }
@@ -184,55 +128,110 @@ public class CRUDPractice {
             e.printStackTrace();
         }
     }
+    class EntireScreen extends JFrame{
+        private Container c = getContentPane();
+        public EntireScreen(){
+            setTitle("GUI CRUD TEST");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    public static void main(String[] args) {
+            c.setLayout(new BorderLayout());
 
-        new EntireScreen();
+            InputHeader inputHeader = new InputHeader();
+            body = new Body();
 
-        CRUDPractice crud = new CRUDPractice();
-        Scanner scanner = new Scanner(System.in);
+            c.add(inputHeader, BorderLayout.NORTH);
+            c.add(body, BorderLayout.CENTER);
 
-        crud.initialize();
+            setSize(1000, 1000);
+            setVisible(true);
+        }
+    }
 
-        while(true){
-            System.out.println("\n1. See all people");
-            System.out.println("2. Add new person");
-            System.out.println("3. Update person");
-            System.out.println("4. Delete person");
-            System.out.println("5. Exit");
-            System.out.print("Enter: ");
-            int choice = scanner.nextInt();
-            scanner.nextLine();
+    class MyPanel extends JPanel{
+        public MyPanel(String name, int age){
+            setLayout(new GridLayout(1, 2));
+            setSize(50, 30);
 
-            if (choice == 1) {
-                System.out.println("\nPerson List:");
-                crud.getPeople();
-            } else if (choice == 2) {
-                System.out.print("Name: ");
-                String name = scanner.nextLine();
-                System.out.print("Age: ");
-                int age = scanner.nextInt();
-                crud.createPerson(name, age);
-                System.out.println("New person added.");
-            } else if (choice == 3){
-                System.out.print("Enter the person name that you want to update: ");
-                String name = scanner.nextLine();
-                System.out.print("Enter the new age: ");
-                int age = scanner.nextInt();
-                crud.updatePerson(name, age);
+            add(new JLabel(name));
+            add(new JLabel(Integer.toString(age)));
+        }
+    }
+    class Body extends JPanel{
+        private JPanel listPanel;
+        public Body(){
+            setLayout(new GridLayout(1, 1));
+            listPanel = new JPanel();
+            listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
+
+            JScrollPane jScrollPane = new JScrollPane(listPanel);
+            add(jScrollPane, BorderLayout.CENTER);
+
+            updateList();
+        }
+
+        public void updateList(){
+            listPanel.removeAll();
+            for(Person p : people){
+                listPanel.add(new MyPanel(p.name, p.age));
             }
-            else if(choice == 4){
-                System.out.print("Enter the person name that you want to delete: ");
-                String name = scanner.nextLine();
-                crud.deletePerson(name);
+            listPanel.revalidate();
+            listPanel.repaint();
+        }
+    }
+    class InputHeader extends JPanel{
+        private String[] choices= {"1. List up", "2. Add", "3. Update", "4. Delete", "5. Exit"};
+        private JRadioButton[] btn = new JRadioButton[5];
+        private ButtonGroup buttonGroup = new ButtonGroup();
+        private JTextField[] jTextFields = new JTextField[2];
+        private JButton submitBtn = new JButton("Enter");
+        public InputHeader(){
+            setSize(500, 100);
+            setLayout(new FlowLayout());
+
+            JLabel jLabel = new JLabel("Enter the Choice, Name, Age");
+
+            for(int i=0; i<5; i++){
+                btn[i] = new JRadioButton(choices[i]);
+                buttonGroup.add(btn[i]);
+                add(btn[i]);
             }
-            else if (choice == 5) {
-                System.out.println("Terminated...");
-                break;
-            } else {
-                System.out.println("Incorrect Input.");
+
+            for(int i=0; i<2; i++){
+                jTextFields[i] = new JTextField(10);
+                add(jTextFields[i]);
+            }
+
+            submitBtn.addMouseListener(new MyMouseListener());
+            add(submitBtn);
+        }
+        class MyMouseListener extends MouseAdapter{
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if(btn[0].isSelected()){
+                    getPeople();
+                    clear();
+                }else if(btn[1].isSelected()){
+                    createPerson(jTextFields[0].getText(), Integer.parseInt(jTextFields[1].getText()));
+                    clear();
+                }else if(btn[2].isSelected()){
+                    updatePerson(jTextFields[0].getText(), Integer.parseInt(jTextFields[1].getText()));
+                    clear();
+                }else if(btn[3].isSelected()){
+                    deletePerson(jTextFields[0].getText());
+                    clear();
+                }else if(btn[4].isSelected()){
+                    System.exit(0);
+                }
             }
         }
-        scanner.close();
+
+        private void clear(){
+            jTextFields[0].setText("");
+            jTextFields[1].setText("");
+        }
+    }
+
+    public static void main(String[] args) {
+        new CRUDPractice();
     }
 }
