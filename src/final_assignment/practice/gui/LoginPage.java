bcbcfc9dfdbf2caf355d46b3ab8b66a409a66916
tlgusdl03 <tlgusdl03@gmail.com>
2024-12-02 18:09:54 +0900

    fix : 로그인 - 메인 - 어드민간 화면 전환 구현

--- src/final_assignment/practice/gui/LoginPage.java
+++ src/final_assignment/practice/gui/LoginPage.java
@@ -1,4 +1,4 @@
-package final_assignment.practice.gui.admin;
+package final_assignment.practice.gui;
 
 import final_assignment.practice.admin.AdminManager;
 
@@ -6,12 +6,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-public class AdminLoginScreen extends JPanel {
+public class LoginPage extends JDialog {
     private JTextField idField = new JTextField(15);
     private JPasswordField passwordField = new JPasswordField(15);
     private JButton loginButton = new JButton("Login");
     private AdminManager adminManager;
-    public AdminLoginScreen(AdminManager adminManager) {
+    public LoginPage(AdminManager adminManager) {
         this.adminManager = adminManager;
 
         setSize(300, 150);
--- src/final_assignment/practice/gui/Router.java
+++ src/final_assignment/practice/gui/Router.java
@@ -3,76 +3,74 @@ package final_assignment.practice.gui;
 import final_assignment.practice.PersonManager;
 import final_assignment.practice.admin.AdminManager;
 import final_assignment.practice.gui.admin.AdminDashboard;
-import final_assignment.practice.gui.admin.AdminLoginScreen;
 import final_assignment.practice.gui.peopleManage.EntireScreen;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
+// 로그인화면 - MainPage간 화면 전환을 담당함
+// 로그인 성공시 MainPage를 볼 수 있도록 함
 public class Router extends JFrame{
-
-    private String[] menuStrings = {"Admin", "Main"};
-    private String[][] subMenuStrings = {
-            {"page"},
-            {"page"},
-    };
-
     private AdminManager adminManager = new AdminManager();
     private PersonManager personManager = new PersonManager();
-    private AdminLoginScreen adminLoginScreen = new AdminLoginScreen(adminManager);
-    private AdminDashboard adminDashboard = new AdminDashboard(adminManager);
+    private LoginPage loginPage = new LoginPage(adminManager);
     private EntireScreen entireScreen = new EntireScreen(personManager);
-    private Object[] object = {adminDashboard, entireScreen};
-    private JMenuBar jMenuBar = new JMenuBar();
-    private JMenu[] menus = new JMenu[2];
+    private AdminDashboard adminDashboard = new AdminDashboard(adminManager);
 
     public Router(){
-        setMenu();
-        setSize(300, 400);
+        init();
+    }
+    void init(){
+        setTitle("Main Application");
+        setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setContentPane(adminLoginScreen);
-        setVisible(true);
+        setMenu();
+        loginPage.setModal(true);
+        loginPage.setLoginButtonActionListener(new LoginBtnListener());
     }
 
     void setMenu(){
-        for(int i=0; i< menus.length; i++){
-            menus[i] = new JMenu(menuStrings[i]);
-            for(int j=0; j< subMenuStrings[i].length; j++){
-                JMenuItem jMenuItem = new JMenuItem(subMenuStrings[i][j]);
-                int finali = i;
-                jMenuItem.addActionListener(
-                    new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                            System.out.println("Page Changed!" + finali);
-                            setContentPane((Container) object[finali]);
-                            revalidate();
-                            repaint();
-                        }
-                    }
-                );
-                menus[i].add(jMenuItem);
+        JMenuBar jMenuBar = new JMenuBar();
+
+        JMenu jMenu1 = new JMenu("Admin");
+        JMenu jMenu2 = new JMenu("Main");
+
+        JMenuItem jMenuItem1 = new JMenuItem("Page");
+        jMenuItem1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setContentPane(adminDashboard);
             }
-            jMenuBar.add(menus[i]);
-        }
+        });
+        JMenuItem jMenuItem2 = new JMenuItem("Page");
+        jMenuItem2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setContentPane(entireScreen);
+            }
+        });
+
+        jMenu1.add(jMenuItem1);
+        jMenu2.add(jMenuItem2);
+
+        jMenuBar.add(jMenu1);
+        jMenuBar.add(jMenu2);
+
         setJMenuBar(jMenuBar);
-        adminLoginScreen.setLoginButtonActionListener(new LoginActionListener());
     }
 
-    class LoginActionListener implements ActionListener{
+    class LoginBtnListener implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println("login button clicked!");
-            if (adminLoginScreen.handleLogin()) {
-                JOptionPane.showMessageDialog(null, "Login successful!", "Success", JOptionPane.INFORMATION_MESSAGE);
-                setContentPane(adminDashboard);
-            }else{
-                JOptionPane.showMessageDialog(null, "Invalid ID or password.", "Login Failed", JOptionPane.ERROR_MESSAGE);
+            if(loginPage.handleLogin()){
+                loginPage.dispose();
+                setContentPane(entireScreen);
+                setVisible(true);
+                revalidate();
+                repaint();
             }
         }
     }
-
-
 }
