113c21297e75d17cd1643feaaccc40998f14370a
tlgusdl03 <tlgusdl03@gmail.com>
2024-12-11 17:37:14 +0900

    feat : 소켓프로그래밍을 이용한 통신 예제

--- src/week_14/ClientEx.java
+++ src/week_14/ClientEx.java
@@ -5,41 +5,39 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ServerEx {
+public class ClientEx {
     public static void main(String[] args) {
         BufferedReader in = null;
         BufferedWriter out = null;
-        ServerSocket listener = null;
         Socket socket = null;
         Scanner scanner = new Scanner(System.in);
         try{
-            listener = new ServerSocket(9999);
-            System.out.println("연결을 기다리고 있습니다....");
-            socket = listener.accept();
-            System.out.println("연결되었습니다.");
+            socket = new Socket("localhost", 9999);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             while (true) {
-                String inputMessage = in.readLine();
-                if (inputMessage.equalsIgnoreCase("bye")) {
-                    System.out.println("클라이언트에서 bye로 연결을 종료하였음");
-                    break;
-                }
-                System.out.println("클라이언트: " + inputMessage);
                 System.out.println("보내기>>");
                 String outputMessage = scanner.nextLine();
+                if (outputMessage.equalsIgnoreCase("bye")) {
+                    out.write(outputMessage + "\n");
+                    out.flush();
+                    break;
+                }
                 out.write(outputMessage + "\n");
                 out.flush();
+                String inputMessage = in.readLine();
+                System.out.println("서버: " + inputMessage);
             }
         }catch (IOException e){
             System.out.println(e.getMessage());
         }finally {
             try {
                 scanner.close();
-                socket.close();
-                listener.close();
+                if (socket != null) {
+                    socket.close();
+                }
             } catch (IOException e) {
-                System.out.println("클라이언트와 채팅 중 오류가 발생했습니다.");
+                System.out.println("서버와 채팅 중 오류가 발생했습니다.");
             }
         }
     }
